ac5ea008b26eb0a854c95776ac4b9f30611c6f28
Dan Stevens <daniel.stevens@oxinst.com>
2022-01-07 13:06:18 +0000

    Added my version of the code following the online tutorial videos

---
+++ src-mine/ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly JsonFileProductService _productService;
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        public IEnumerable<Product> Products { get; private set; }
+
+        public void OnGet()
+        {
+            this.Products = _productService.GetProducts();
+        }
+    }
+}
